0733e47bf60298254079d9bf987705a5c78e6393
hanashiro <felipeaah@gmail.com>
2020-06-04 21:51:04 -0300

    content type

--- src/app/services/tw-dev-server.service.ts
+++ src/app/services/tw-dev-server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +18,13 @@ export class TwDevServerService {
   }
 
   sendCode(url, method, body){
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
     if(method === 'GET'){
       return this.http.get(url);
     }else if(method === 'POST'){
-      return this.http.post(url, body);
+      return this.http.post(url, body, {headers});
     }else if(method === 'PUT'){
-      return this.http.put(url, body);
+      return this.http.put(url, body, {headers});
     }else if(method === 'DELETE'){
       return this.http.delete(url);
     }
