b0f2e6f5073a51c6afcd1a094a51da332ed2aa2b
hanashiro <felipeaah@gmail.com>
2021-01-29 19:47:57 -0300

    next config update

---
+++ _next/static/v127oT-QpC9m2mTDHUgrL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7ff1e66679741863ea2c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-154b2ec68bf9e7204aeb.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
