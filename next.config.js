7246ebd3b8f8d96ab622ffa3003c483e85f1cca7
hanashiro <felipeaah@gmail.com>
2021-01-29 19:47:01 -0300

    next config update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const debug = process.env.NODE_ENV !== 'production';
 module.exports = {
-    basePath: '/tw-dev-server',
+    //basePath: '/tw-dev-server',
+    assetPrefix: !debug ? '/tw-dev-server/' : '',
 };
